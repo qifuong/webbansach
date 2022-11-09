eb6def67449d08d69a03cbaa888026df7b6cef01
qifuong <lequyphuong330@gmail.com>
2022-11-09 22:37:34 +0700

    bansach

---
+++ contact.php
@@ -0,0 +1,47 @@
+<?php 
+	include "head.php"
+	?>
+<?php
+$title ="Shop huy";
+$name ="Điện thoai";
+?>
+<?php 
+	include "top.php"
+    ?>
+    <?php 
+	include "Header.php"
+	?>
+	<?php 
+	include "navigation.php"
+	?>
+	<!--//////////////////////////////////////////////////-->
+	<!--///////////////////Contact Page///////////////////-->
+	<!--//////////////////////////////////////////////////-->
+	<div id="page-content" class="single-page">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<ul class="breadcrumb">
+						<li><a href="index.php">Home</a></li>
+						<li><a href="contact.php">Contact</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="heading"><h1>Liên hệ với chúng tôi</h1></div>
+				</div>
+				<div class="col-md-6">
+					<p><span class="glyphicon glyphicon-home"></span>Lê Quý Phương</p>
+					<p><span class="glyphicon glyphicon-earphone"></span> 0949958573</p>
+					<p><span class="glyphicon glyphicon-envelope"></span> quyphuongg@gmail.com</p>
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d979.6910699492774!2d106.70228512921813!3d10.829342716739175!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3175288aeb026b6b%3A0x3f4277d164ccc0b2!2zxJAuIMSQ4bq3bmcgVGh14buzIFRyw6JtLCBwaMaw4budbmcgMTMsIELDrG5oIFRo4bqhbmgsIFRow6BuaCBwaOG7kSBI4buTIENow60gTWluaCwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1666428189149!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php 
+	include "footer.php"
+	?>
+</body>
+</html>
